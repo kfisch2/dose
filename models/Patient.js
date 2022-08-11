ab08f6d140428cd3bbc91536e81fd56a252e1f1b
Kayla Fischer <102554319+kfisch2@users.noreply.github.com>
2022-08-10 21:41:06 -0700

    Update Patient.js
    weird merge issue duplicated fields... so I fixed it here while Robin was present

--- models/Patient.js
+++ models/Patient.js
@@ -38,49 +38,28 @@ Patient.init(
             },
         },
     },
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    email: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      unique: true,
-      validate: {
-        isEmail: true,
-      },
-    },
-    // password column
-    password: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      validate: {
-        len: [8],
-      },
-    },
-  },
-  {
-    // hooks: {
-    //   async beforeCreate(newPatientData) {
-    //     newPatientData.password = await bcrypt.hash(
-    //       newPatientData.password,
-    //       10
-    //     );
-    //     return newPatientData;
-    //   },
-    //   async beforeUpdate(updatedPatientData) {
-    //     updatedPatientData.password = await bcrypt.hash(
-    //       updatedPatientData.password,
-    //       10
-    //     );
-    //     return updatedPatientData;
-    //   },
-    // },
-    sequelize,
-    freezeTableName: true,
-    underscored: true,
-    modelName: 'patient',
-  }
+    {
+        // hooks: {
+        //   async beforeCreate(newPatientData) {
+        //     newPatientData.password = await bcrypt.hash(
+        //       newPatientData.password,
+        //       10
+        //     );
+        //     return newPatientData;
+        //   },
+        //   async beforeUpdate(updatedPatientData) {
+        //     updatedPatientData.password = await bcrypt.hash(
+        //       updatedPatientData.password,
+        //       10
+        //     );
+        //     return updatedPatientData;
+        //   },
+        // },
+        sequelize,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'patient',
+    }
 );
 
 module.exports = Patient;
