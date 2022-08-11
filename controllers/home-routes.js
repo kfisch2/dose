1e17ec50dca4d25ff02755a05ed5e0553eb3b481
bg <62928419+bgeertsen@users.noreply.github.com>
2022-08-10 20:15:51 -0700

    added login route

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,33 +1,46 @@
 const router = require('express').Router();
 const sequelize = require('../config/connection');
-const { Patient, Prescription, Diagnosis } = require("../models");
+const { Patient, Prescription, Diagnosis } = require('../models');
 
 router.get('/', (req, res) => {
-  Patient.findAll({
-    attributes: [
-      "id",
-      "name",
-      "email"
-    ],
-    include: [
-      {
-        model: Diagnosis,
-        attributes: ["id", "name", "patient_id"],
-        include: {
-          model: Prescription,
-          attributes: ["id", "medication", "refill_date", "date_prescribed", "cost"]
-        }
-      }
-    ]
-  })
-  .then((dbPatientData) => {
-    const patients = dbPatientData.map((post) => post.get({ plain: true }));
-    res.render('main', patients);
-  })
-  .catch((err) => {
-    console.log(err);
-    res.status(500).json(err);
-  })
+    Patient.findAll({
+        attributes: ['id', 'name', 'email'],
+        include: [
+            {
+                model: Diagnosis,
+                attributes: ['id', 'name', 'patient_id'],
+                include: {
+                    model: Prescription,
+                    attributes: [
+                        'id',
+                        'medication',
+                        'refill_date',
+                        'date_prescribed',
+                        'cost',
+                    ],
+                },
+            },
+        ],
+    })
+        .then((dbPatientData) => {
+            const patients = dbPatientData.map((post) =>
+                post.get({ plain: true })
+            );
+            res.render('main', patients);
+        })
+        .catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+router.get('/login', (req, res) => {
+    if (req.session.loggedIn) {
+        res.redirect('/');
+        return;
+    }
+
+    res.render('/');
 });
 
 module.exports = router;
