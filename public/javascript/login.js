88a352029e0e755ad9089cc7b9fae256de9f9a10
stmaddox <sean.maddox89@yahoo.com>
2022-08-10 22:30:57 -0700

    login WIP

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -1,9 +1,13 @@
-async function loginForm(event) {
-    event.preventDefault();
+// async function loginForm(event) {
+//     event.preventDefault();
 
-    // username and password 
-    const username = document.querySelector('#login-username').value.trim();
-    const password = document.querySelector('#login-pw').value.trim();
+//     // username and password 
+//     const username = document.querySelector('#login-username').value.trim();
+//     const password = document.querySelector('#login-pw').value.trim();
 
+//     if (username && password) {
+//         const response = await fetch('/')
+//     }
+// }
 
-}
+// **Do we need a user-route for the login or with it work off of the home-routes?**
