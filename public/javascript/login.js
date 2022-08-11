e774e531e9c874b90d2024a901a0031cde0b577c
stmaddox <sean.maddox89@yahoo.com>
2022-08-10 20:28:47 -0700

    added login handlebar and working on login.js

---
+++ public/javascript/login.js
@@ -0,0 +1,9 @@
+async function loginForm(event) {
+    event.preventDefault();
+
+    // username and password 
+    const username = document.querySelector('#login-username').value.trim();
+    const password = document.querySelector('#login-pw').value.trim();
+
+
+}
