aea036ed15263e56eae77b96c9af10d1ba99ad4f
bg <62928419+bgeertsen@users.noreply.github.com>
2022-08-10 19:16:29 -0700

    added home routes

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,8 +1,33 @@
 const router = require('express').Router();
 const sequelize = require('../config/connection');
+const { Patient, Prescription, Diagnosis } = require("../models");
 
 router.get('/', (req, res) => {
-  res.send('We made it to the homepage!');
+  Patient.findAll({
+    attributes: [
+      "id",
+      "name",
+      "email"
+    ],
+    include: [
+      {
+        model: Diagnosis,
+        attributes: ["id", "name", "patient_id"],
+        include: {
+          model: Prescription,
+          attributes: ["id", "medication", "refill_date", "date_prescribed", "cost"]
+        }
+      }
+    ]
+  })
+  .then((dbPatientData) => {
+    const patients = dbPatientData.map((post) => post.get({ plain: true }));
+    res.render('main', patients);
+  })
+  .catch((err) => {
+    console.log(err);
+    res.status(500).json(err);
+  })
 });
 
 module.exports = router;
