0dbd9c6552cef852502c5dc24ffb401dc2a2b8bd
stmaddox <sean.maddox89@yahoo.com>
2022-08-14 16:53:17 -0700

    delete.js WIP

--- public/javascript/add-rx.js
+++ public/javascript/add-rx.js
@@ -7,5 +7,25 @@ async function addRx(event) {
     const refillDate = document.querySelector('input[name="refill-date"]').value;
     const diagnosis = document.querySelector('input[name="med-diag"]').value;
 
-    
+    const response = await fetch(`/api/prescriptions`, {
+        method: 'POST',
+        body: JSON.stringify({
+            medication,
+            cost,
+            fillDate,
+            refillDate,
+            diagnosis,
+        }),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    });
+
+    if (response.ok) {
+        document.location.replace('/dashboard');
+    } else {
+        alert(response.statusText);
+    }  
 }
+
+document.querySelector('.new-rx').addEventListener('submit', addRx);
---
+++ public/javascript/delete-rx.js
@@ -0,0 +1,7 @@
+async function deleteRx(event) {
+    event.preventDefault();
+
+    const id = window.location.toString().split('/')[
+        window.location.toString().split('/').length-1
+    ];
+}
