66b3c425b50b8fe37c0ea8bbdf3e5f7cbd240073
bg <62928419+bgeertsen@users.noreply.github.com>
2022-08-14 15:39:11 -0700

    scheduled text reminder now sends 3 days prior to refill date

--- controllers/twilio-routes.js
+++ controllers/twilio-routes.js
@@ -8,11 +8,11 @@ const client = require('twilio')(
     process.env.TWILIO_AUTH
 );
 
-function sendTextNotification() {
+function sendTextNotification(phone) {
     client.messages
         .create({
             body: 'Your prescription needs to be refilled in 3 days',
-            to: `+1${process.env.PHONE_RECEIVER}`,
+            to: `+1${phone}`,
             from: process.env.PHONE_SENDER,
         })
         .then((message) => console.log(message))
@@ -27,18 +27,16 @@ router.get('/', async (req, res, next) => {
     res.status(200).json({
         results,
     });
-    console.log('refill date: ', results[0].refill_date);
     for (i = 0; i < results.length; i++) {
-        const refillDate = results[i].refill_date;
-        const day = refillDate.split('-')[2];
-        const month = refillDate.split('-')[1];
-        // const day = 13;
-        // const month = 8;
-        console.log(`Scheduled reminders for ${month}/${day}`);
+        let reminderDate = new Date(results[i].refill_date);
+        console.log(reminderDate);
+        reminderDate.setDate(reminderDate.getDate() - 2);
+        const day = reminderDate.getDate();
+        const month = reminderDate.getMonth() + 1;
 
         cron.schedule(`00 12 ${day} ${month} *`, () => {
             console.log('schedule test');
-            //sendTextNotification();
+            sendTextNotification(process.env.PHONE_RECEIVER);
         });
     }
 });
