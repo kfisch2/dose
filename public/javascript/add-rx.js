0de46847583e575342a404e0d1f87fbcccfa4c32
stmaddox <sean.maddox89@yahoo.com>
2022-08-14 16:15:48 -0700

    add-rx.js WIP

--- public/javascript/add-rx.js
+++ public/javascript/add-rx.js
@@ -1,5 +1,9 @@
 async function addRx(event) {
     event.preventDefault();
 
-    const 
+    const medication = document.querySelector('input[name="rx-name"]').value;
+    const cost = document.querySelector('input["rx-cost"]').value;
+    const fillDate = document.querySelector('input[name="fill-date"]')
+    const medication = document.querySelector('input[name="rx-name"]').value;
+    const medication = document.querySelector('input[name="rx-name"]').value;
 }
