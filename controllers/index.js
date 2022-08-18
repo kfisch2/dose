5fe492ab666903fe850cf7c63f3d278c6fa4c40e
kfisch2 <kmlindsey24@gmail.com>
2022-08-17 19:06:50 -0700

    twilio routes

--- controllers/index.js
+++ controllers/index.js
@@ -3,11 +3,11 @@ const router = require('express').Router();
 const apiRoutes = require('./api/');
 const homeRoutes = require('./home-routes.js');
 const dashboardRoutes = require('./dashboard-routes.js');
-// const twilioRoutes = require('./twilio-routes');
+const twilioRoutes = require('./twilio-routes');
 
 router.use('/', homeRoutes);
 router.use('/api', apiRoutes);
 router.use('/dashboard', dashboardRoutes);
-// router.use('/twilio', twilioRoutes);
+router.use('/twilio', twilioRoutes);
 
 module.exports = router;
